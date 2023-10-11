fcc0a375a5dc17b0f0195bd7d3dcd03f4c4d09df
sukanyagurav <sukanyagurav6@gmail.com>
2023-10-12 00:18:09 +0530

    update logic

--- script.js
+++ script.js
@@ -12,12 +12,26 @@ let error=false;
 btn.addEventListener('click',function(){
     calculateAge(day,month,year)
 })
-
+// only numbers can enter
+const getStringWithNumbersOnly = (str) => [...str].filter((v) => Number.isInteger(+v) && v !== ' ').join('');
+  
+function cleareFields(){
+    resultYearLabel.innerHTML= '- -' 
+    resultMonthLabel.innerHTML= '- -'
+    resultDayLabel.innerHTML= '- -'
+    dayEle.disabled=false
+    monthEle.disabled=false
+    yearEle.disabled=false
+    dayEle.value=''
+    monthEle.value=''
+    yearEle.value=''
+}
 function calculateAge(day,month,year){
     const date=new Date();
     let currentDay= date.getDate();
     let currentMonth=date.getMonth()+1;
     let currentYear= date.getFullYear();
+    let daysInEnteredMonth = new Date(+year.value, +month.value, 0).getDate()
     let daysInMonth = new Date(currentYear, currentMonth, 0).getDate()
     if(!day.value){
         emptyErrorMessage(day)
@@ -31,71 +45,87 @@ function calculateAge(day,month,year){
     dayEle=day
     monthEle=month
     yearEle=year
-    day=day.value;
-    month=month.value;
-    year=year.value;
+    day=+day.value;
+    month=+month.value;
+    year=+year.value;
+    let isValidDay=isValidMonth=isValidYear=true
     if (!dayEle.value || !monthEle.value || !yearEle.value) {
         return;
     }
     // year
-    if(currentYear < year ){
+    if(currentYear < year  ){
         yearEle.parentElement.classList.add('show')
         yearEle.parentElement.querySelector('span').innerHTML='Must be in the past'
-    }else{
+        isValidYear=false
+    }else if(year < 1925){
+        yearEle.parentElement.classList.add('show')
+        yearEle.parentElement.querySelector('span').innerHTML='Year must be after 1925'
+        isValidYear=false
+    }
+    else{
         yearEle.parentElement.classList.remove('show')
         if(month > currentMonth){
             currentYear=currentYear - 1;
         }
-        currentYear = currentYear - year
-        resultYearLabel.innerHTML= currentYear 
+        isValidYear=true
     }
     
     //Month
     if(month <= 0 || month > 12 ){
         monthEle.parentElement.classList.add('show')
         monthEle.parentElement.querySelector('span').innerHTML='Must be a valid month'
+        isValidMonth=false
     }else{
         monthEle.parentElement.classList.remove('show')
+        isValidMonth=true
     }
+
     if(day > currentDay){
-        currentMonth =currentMonth -1 
+        currentMonth =currentMonth - 1 
     }
     if(month > currentMonth){
         currentMonth=currentMonth + 12
     }
-    currentMonth= currentMonth - month
-    resultMonthLabel.innerHTML= currentMonth
-    
     //days
-    if(day <= 0 || day > daysInMonth){
+    if(day <= 0 || day > daysInEnteredMonth){
         dayEle.parentElement.classList.add('show')
-        dayEle.parentElement.querySelector('span').innerHTML='Must be a valid month'
+        dayEle.parentElement.querySelector('span').innerHTML='Must be a valid day'
+        isValidDay=false
     }else{
         dayEle.parentElement.classList.remove('show')
+        isValidDay=true
     }
     if(day > currentDay){
-        currentDay=currentDay + daysInMonth 
+        currentDay=currentDay + daysInMonth -1
     }
-    day=currentDay - day
-    resultDayLabel.innerHTML= day
-    setTimeout(()=>{
-        resultYearLabel.innerHTML= '- -' 
-        resultMonthLabel.innerHTML= '- -'
-        resultDayLabel.innerHTML= '- -'
-        dayEle.value=''
-        monthEle.value=''
-        yearEle.value=''
-    },9000)
+    if(isValidDay && isValidMonth && isValidYear){
+        currentYear = currentYear - year
+        resultYearLabel.innerHTML= currentYear
+        currentMonth= currentMonth - month
+        resultMonthLabel.innerHTML= currentMonth
+        day=currentDay - day
+        resultDayLabel.innerHTML= day
+        dayEle.disabled=true
+        monthEle.disabled=true
+        yearEle.disabled=true
+      setTimeout(()=>{
+       cleareFields()
+    },5000)
+    }
+
 }
 
-day.addEventListener('input',function(){
-    emptyErrorMessage(day)
+day.addEventListener('input',function(e){
+   day.value= getStringWithNumbersOnly(e.target.value)
+    !day.value && emptyErrorMessage(day)
 })
 month.addEventListener('input',function(){
-    emptyErrorMessage(month)
+    month.value= getStringWithNumbersOnly(e.target.value)
+    !month.value && emptyErrorMessage(day)
 })
 year.addEventListener('input',function(){
-    emptyErrorMessage(year)
+    year.value= getStringWithNumbersOnly(e.target.value)
+    !year.value && emptyErrorMessage(year)
 })
 
 function emptyErrorMessage(element){
